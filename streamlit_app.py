92a9a5c653c38fd5eeee0e1f528447defe491e4c
gfjsieoposk <aleks.w.karp@gmail.com>
2025-03-02 20:33:31 -0800

    Update streamlit_app.py
    Got rid of cnx

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,8 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-import snowflake.connector
 import streamlit as st
-session = cnx.session()
+import snowflake.connector
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
